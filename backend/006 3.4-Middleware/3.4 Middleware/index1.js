f1fab463c0bcbfc8b763b2c87a7ad8f7912e2926
sowmik-sec <sowmik.sec@gmail.com>
2024-05-06 21:13:20 +0600

    custom middlewares

--- backend/006 3.4-Middleware/3.4 Middleware/index1.js
+++ backend/006 3.4-Middleware/3.4 Middleware/index1.js
@@ -1,18 +1,13 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-function logger(req, res, next) {
-  console.log("Request Method: ", req.method);
-  console.log("Request URL: ", req.url);
-  next();
-}
-
-app.use(logger);
-
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
--- backend/006 3.4-Middleware/3.4 Middleware/solution2.js
+++ backend/006 3.4-Middleware/3.4 Middleware/solution2.js
@@ -1,15 +1,9 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
-
-function logger(req, res, next) {
-  console.log("Request Method: ", req.method);
-  console.log("Request URL: ", req.url);
-  next();
-}
-
-app.use(logger);
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
