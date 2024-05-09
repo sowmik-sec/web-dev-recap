2f1ef3597e3c1feaad4d0dc0f9b4524590cfbec1
sowmik-sec <sowmik.sec@gmail.com>
2024-05-09 12:46:52 +0600

    DELETE Specific joke

--- backend/6.0 DIY API/index.js
+++ backend/6.0 DIY API/index.js
@@ -68,6 +68,18 @@ app.patch("/jokes/:id", (req, res) => {
 });
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+});
 
 //8. DELETE All jokes
 
