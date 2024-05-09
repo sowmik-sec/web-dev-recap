1bdf998c7a4b83982a6544010779f49914714ad1
sowmik-sec <sowmik.sec@gmail.com>
2024-05-09 12:32:50 +0600

    GET a specific joke

--- backend/6.0 DIY API/index.js
+++ backend/6.0 DIY API/index.js
@@ -14,6 +14,11 @@ app.get("/random", (req, res) => {
 });
 
 //2. GET a specific joke
+app.get("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+});
 
 //3. GET a jokes by filtering on the joke type
 
