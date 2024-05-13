a743d3e151c283b93d59b5ce162af0b7f18e6a39
sowmik-sec <sowmik.sec@gmail.com>
2024-05-13 17:35:54 +0600

    set db and create get api for visited countries

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -1,5 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "asdf12",
+  port: 5432,
+});
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -8,7 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("select country_code from visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
