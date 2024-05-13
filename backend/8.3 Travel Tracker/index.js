2b721cf95f0b768b6237db14c876c9743b17656c
sowmik-sec <sowmik.sec@gmail.com>
2024-05-13 18:05:18 +0600

    create post request for adding new country

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -28,6 +28,22 @@ app.get("/", async (req, res) => {
   db.end();
 });
 
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const result = await db.query(
+    "select country_code from countries where country_name = $1",
+    [input]
+  );
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    await db.query("insert into visited_countries (country_code) values ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
