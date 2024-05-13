ad7861f25d87ba6354e279589030ccae027678ed
sowmik-sec <sowmik.sec@gmail.com>
2024-05-13 18:20:31 +0600

    handle exception in backend

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+const app = express();
+const port = 3000;
+
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -11,36 +14,58 @@ const db = new pg.Client({
 });
 db.connect();
 
-const app = express();
-const port = 3000;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  const result = await db.query("select country_code from visited_countries");
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
-  res.render("index.ejs", { countries, total: countries.length });
-  db.end();
+  return countries;
+}
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
+//INSERT new country
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
-  const result = await db.query(
-    "select country_code from countries where country_name = $1",
-    [input]
-  );
-  if (result.rows.length !== 0) {
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
     const data = result.rows[0];
     const countryCode = data.country_code;
-    await db.query("insert into visited_countries (country_code) values ($1)", [
-      countryCode,
-    ]);
-    res.redirect("/");
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
