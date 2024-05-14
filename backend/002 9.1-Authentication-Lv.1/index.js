c005d0fdf9331161613529c847f61e45ddd61380
sowmik-sec <sowmik.sec@gmail.com>
2024-05-14 10:23:54 +0600

    project setup and register with email and password

---
+++ backend/002 9.1-Authentication-Lv.1/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "asdf12",
+  port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  const result = await db.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2)",
+    [email, password]
+  );
+  console.log(result);
+  res.render("secrets.ejs");
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
